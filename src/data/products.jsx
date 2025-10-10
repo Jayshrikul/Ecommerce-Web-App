a2b524784a65e8210dd5a26cf8706d2636b37c18
Jayshri Kulkarni <jayshrikulkarni1999@ .com>
2025-10-11 01:14:48 +0530

    delete package.json dummy file

--- src/data/products.jsx
+++ src/data/products.jsx
@@ -17,8 +17,9 @@ import cloth2 from "../assets/Clothing/trenchcoat.jpg";
 import cloth3 from "../assets/Clothing/formalset2.jpg";
 // ------- Accessories -------
 import acc1 from "../assets/accessories/jwelesset1.jpg";
-import acc2 from "../assets/accessories/shoulderbag.jpg";
+import acc2 from "../assets/accessories/shoulderbag-new.jpg";
 import acc3 from "../assets/accessories/DiamondRing.jpg"
+
 
 //---------Footwear --------
 import shoes1 from "../assets/Footwear/Beautifulsandal.jpg";
