f4134388d5a4188d442951daad384cfd2b6b8b43
Jayshri Kulkarni <jayshrikulkarni1999@ .com>
2025-10-11 01:19:36 +0530

    fix importin vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // ✅ ensures Vercel knows where to find the build
+    outDir: 'dist',
   },
   resolve: {
     alias: {
-      '@': '/src', // optional, but nice for cleaner imports
+      '@': '/src',
     },
   },
 })
